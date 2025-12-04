8f69d0b0f3bbe705d8e78092be3ac361de18c216
Maha6468 <motherma7633@gmail.com>
2025-12-05 02:05:06 +0600

    .....

---
+++ lib/ui/screens/progress_task_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/task_card.dart';
+import '../widgets/tm_app_bar.dart';
+
+class ProgressTaskScreen extends StatefulWidget {
+  const ProgressTaskScreen({super.key});
+
+  @override
+  State<ProgressTaskScreen> createState() => _ProgressTaskScreenState();
+}
+
+class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TMAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: ListView.separated(
+
+            itemBuilder: (context,index){
+              return TaskCard(status: 'Progress', cardColor: Colors.purple,);
+            },
+            separatorBuilder: (context,index){
+              return SizedBox(height: 2,);
+            },
+            itemCount: 10),
+      ),
+    );
+  }
+}
