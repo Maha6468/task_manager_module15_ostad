7db7e9ce7954dad7290b8d5240cbc43230691a49
Maha6468 <motherma7633@gmail.com>
2025-12-05 02:04:22 +0600

    .....

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,7 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'app.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      //home:
-    );
-  }
+void main(){
+  runApp(TaskManagerApp());
 }
---
+++ lib/ui/screens/main_nav_bar_holder_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager_module15_ostad/ui/screens/progress_task_screen.dart';
+import 'cancle_task_screen.dart';
+import 'completed_task_screen.dart';
+import 'new_task_screen.dart';
+
+class MainNavBarHolderScreen extends StatefulWidget {
+  const MainNavBarHolderScreen({super.key});
+
+  @override
+  State<MainNavBarHolderScreen> createState() => _MainNavBarHolderScreenState();
+}
+
+class _MainNavBarHolderScreenState extends State<MainNavBarHolderScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> _screens = [
+    NewTaskScreen(),
+    CompletedTaskScreen(),
+    CancleTaskScreen(),
+    ProgressTaskScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: NavigationBar(
+          selectedIndex: _selectedIndex,
+          onDestinationSelected: (int index) {
+            _selectedIndex = index;
+            setState(() {});
+          },
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.refresh), label: 'New Task'),
+            NavigationDestination(
+                icon: Icon(Icons.done_all), label: 'Completed'),
+            NavigationDestination(icon: Icon(Icons.close), label: 'Cancelled'),
+            NavigationDestination(icon: Icon(Icons.refresh), label: 'Progress'),
+          ]),
+    );
+  }
+}
