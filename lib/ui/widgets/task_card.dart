e40ffeadd4c4d064c58b73ace0ce048fc9bc2212
Maha6468 <motherma7633@gmail.com>
2025-12-05 02:05:25 +0600

    .....

---
+++ lib/ui/widgets/task_card.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class TaskCard extends StatelessWidget {
+  const TaskCard({
+    super.key, required this.status, required this.cardColor,
+  });
+
+  final String status;
+  final Color cardColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Card(
+        color: Colors.white,
+        child: ListTile(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          title: Text('This is task title',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                fontSize: 18
+            ),
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Description of task'),
+              Text('Date: 27/11/2025'),
+              Row(
+                children: [
+                  Chip(
+                    label: Text(status),
+                    backgroundColor: cardColor,
+                    labelStyle: TextStyle(color: Colors.white),
+                    padding: EdgeInsets.symmetric(horizontal: 18),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                  ),
+                  Spacer(),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.edit_note_rounded,
+                        color: Colors.green,
+                      )),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.delete,
+                        color: Colors.red,
+                      )),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
